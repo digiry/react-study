3ebe24be061d06ef918b7016db3eb6f3283a0f56
Kyujin Kang <digiry@gmail.com>
2017-11-01 02:06:05 +0900

    Add memo-app 3.5

--- memo-app/src/modules/memo.js
+++ memo-app/src/modules/memo.js
@@ -1,10 +1,32 @@
-import { handleActions } from "redux-actions";
-import { Map } from "immutable";
+import { createAction ,handleActions } from "redux-actions";
+import { Map, List, fromJS } from "immutable";
+import { pender } from "redux-pender";
+import * as WebAPI from 'lib/web-api';
+
+const CREATE_MEMO = 'memo/CREATE_MEMO';
+const GET_INITIAL_MEMO = 'memo/GET_INITIAL_MEMO';
+const GET_RECENT_MEMO = 'memo/GET_RECENT_MEMO';
+
+export const createMemo = createAction(CREATE_MEMO, WebAPI.createMemo);
+export const getInitialMemo = createAction(GET_INITIAL_MEMO, WebAPI.getInitialMemo);
+export const getRecentMemo = createAction(GET_RECENT_MEMO, WebAPI.getRecentMemo);
 
 const initialState = Map({
-
+  data: List()
 });
 
 export default handleActions({
-
+  // 초기 메모 로딩
+  ...pender({
+    type: GET_INITIAL_MEMO,
+    onSuccess: (state, action) => state.set('data', fromJS(action.payload.data))
+  }),
+  // 신규 메모 로딩
+  ...pender({
+    type: GET_RECENT_MEMO,
+    onSuccess: (state, action) => {
+      const data = state.get('data');
+      return state.set('data', fromJS(action.payload.data).concat(data))
+    }
+  })
 }, initialState);
