8fdc9510cc9a42954974893b96bcdeca288f0be3
Kyujin Kang <digiry@gmail.com>
2017-12-24 02:38:24 +0900

    Add 6 2-6 chapter

--- heurm-server/src/api/books/books.controller.js
+++ heurm-server/src/api/books/books.controller.js
@@ -1,20 +1,146 @@
-exports.list = (ctx) => {
-  ctx.body = 'listed';
+const Book = require('models/book');
+const Joi = require('joi'); 
+// const ObjectId = require('mongoose').Types.ObjectId;
+const { Types: { ObjectId } } = require('mongoose');
+
+exports.list = async (ctx) => {
+  let books;
+
+  try {
+    books = await Book.find()
+      .sort({_id: -1})
+      .limit(3)
+      .exec();
+  } catch(e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = books;
 };
 
-exports.create = (ctx) => {
-  ctx.body = 'created';
+exports.get = async (ctx) => {
+  const { id } = ctx.params;
+
+  let book;
+
+  try {
+    book = await Book.findById(id).exec();
+  } catch(e) {
+    if (e.name === 'CastError') {
+      ctx.status = 400;
+      return;
+    }
+    return ctx.throw(500, e);
+  }
+
+  if (!book) {
+    ctx.status = 404;
+    ctx.body = { message: 'book not found' };
+    return;
+  }
+
+  ctx.body = book;
 };
 
-exports.delete = (ctx) => {
-  ctx.body = 'deleted';
+exports.create = async (ctx) => {
+  const {
+    title,
+    authors,
+    publishedDate,
+    price,
+    tags
+  } = ctx.request.body;
+
+  const book = new Book({
+    title,
+    authors,
+    publishedDate,
+    price,
+    tags
+  });
+
+  try {
+    await book.save();
+  } catch(e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = book;
 };
 
-exports.replace = (ctx) => {
-  ctx.body = 'replaced';
+exports.delete = async (ctx) => {
+  const { id } = ctx.params;
+
+  try {
+    await Book.findByIdAndRemove(id).exec();
+  } catch(e) {
+    if (e.name === 'CastError') {
+      ctx.status = 400;
+      return;
+    }
+  }
+
+  ctx.status = 204;
 };
 
-exports.update = (ctx) => {
-  ctx.body = 'updated';
+exports.replace = async (ctx) => {
+  const { id } = ctx.params;
+
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    authors: Joi.array().items(Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required()
+    })),
+    publishedDate: Joi.date().required(),
+    price: Joi.number().required(),
+    tags: Joi.array().items(Joi.string()).required()
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  let book;
+
+  try {
+    book = await Book.findByIdAndUpdate(id, ctx.request.body, {
+      upsert: true,
+      new: true
+    });
+  } catch(e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = book;
 };
 
+exports.update = async (ctx) => {
+  const { id } = ctx.params;
+
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+
+  let book;
+
+  try {
+    book = await Book.findByIdAndUpdate(id, ctx.request.body, {
+      new: true
+    });
+  } catch(e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = book;
+};
--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
@@ -5,33 +7,29 @@ const app = new Koa();
 const router = new Router();
 const api = require('./api');
 
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useMongoClient: true
+}).then(
+  (response) => {
+    console.log('Successfully connectd to mongodb');
+  }
+).catch(e => {
+  console.error(e);
+});
+
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser());
+
 router.use('/api', api.routes());
 
-router.get('/', (ctx, next) => {
-  ctx.body = '홈';
-});
+app.use(router.routes()).use(router.allowedMethods());
 
-router.get('/about', (ctx, next) => {
-  ctx.body = '소개';
-});
-
-router.get('/about/:name', (ctx, next) => {
-  const { name } = ctx.params;
-  ctx.body = name + '의 소개';
-});
-
-router.get('/post', (ctx, next) => {
-  const { id } = ctx.request.query;
-  if (id) {
-    ctx.body = '포스트 #' + id;
-  } else {
-    ctx.body = '포스트 아이디가 없습니다.';
-  }
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-app.listen(4000, () => {
-  console.log('heurm server is listening to port 4000');
+app.listen(port, () => {
+  console.log('heurm server is listening to port ' + port);
 });
